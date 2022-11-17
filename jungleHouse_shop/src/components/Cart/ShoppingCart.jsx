20944113b0c0a43a67a1e7f20d058a52f6632d97
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-11-17 11:40:15 +0100

    animation of card and menu is done. the card still need style in tablet mode

--- jungleHouse_shop/src/components/Cart/ShoppingCart.jsx
+++ jungleHouse_shop/src/components/Cart/ShoppingCart.jsx
@@ -40,39 +40,51 @@ export const ShoppingCart = ({cart, updateCart}) =>{
     }
 
 
-    return isCartOpen ?(
+    return <>
+        {isCartOpen ?
         <>
             <div>
                 <img alt ="Shopping Cart" className="relative h-8 cursor-pointer" src={cartImg} onClick={()=>setIsCartOpen(false)}/>
                 {cart.length > 0 ? <span className="absolute bottom-5 bg-green-logo-light h-5 w-5 rounded-full flex flex-row justify-center">{cart.length}</span> : null}
             </div>
-            <div className="top-20 shadow-lg shadow-gray-300 bg-white w-screen md:w-1/2 lg:w-1/3 h-[95vh] right-0 absolute
-            flex flex-col
-            overflow-y-auto">
+        </>
+    :
+        <>
+            <div>
+                <img alt ="Shopping Cart" className="relative h-8 cursor-pointer" src={cartImg} onClick={()=>setIsCartOpen(true)}/>
+                {cart.length > 0 ? <span className="absolute shadow-sm shadow-black bottom-5 bg-green-logo-light h-5 w-5 rounded-full flex flex-row justify-center">{cart.length}</span> : null}
+            </div>
+        </>}
 
-                <div className="bg-button-return bg-cover h-16 flex p-7 flex-none">
-                    <button onClick={()=>setIsCartOpen(false)} className='flex gap-2 self-center'>
-                        <img src={close} alt="close button" className="h-8"/>
-                        <p className="text-white text-xl font-bold">Continuer mes achats</p>
-                    </button>
+        <div className={`top-16 shadow-lg shadow-gray-300 bg-white w-screen md:w-1/2 lg:w-1/3 h-[95vh] right-0 absolute
+            flex flex-col
+            overflow-y-auto
+            ${isCartOpen? 'translate-x-0' : 'translate-x-full'} transition-all ease-in-out duration-700`}>
+
+            <div className="bg-button-return bg-cover h-16 flex p-7 flex-none">
+                <button onClick={()=>setIsCartOpen(false)} className='flex gap-2 self-center'>
+                    <img src={close} alt="close button" className="h-8"/>
+                    <p className="text-white text-xl font-bold">Continuer mes achats</p>
+                </button>
+            </div>
+
+
+            <div className="cart p-7 flex-1">
+                <div className="flex flex-row justify-between">
+                    <h3 className='text-2xl font-bold'>Panier</h3>
+                    <p className='text-2xl font-bold text-gray-400'>{cart.length}{cart.length <= 1 ? " Produit" : " Produits"}</p>
                 </div>
 
+                {cart.length > 0 ? (
+                    <div className="">
+                        <ul className="h-[550px] overflow-scroll my-4 bg-gray-100">
+                            {cart.map(({id, image, name, price, amount }, index) => (
+                                <div key={`${name}-${index}`} className="border-b-2 p-4">
 
-                <div className="cart p-7 flex-1">
-                    <div className="flex flex-row justify-between">
-                        <h3 className='text-2xl font-bold'>Panier</h3>
-                        <p className='text-2xl font-bold text-gray-400'>{cart.length}{cart.length <= 1 ? " Produit" : " Produits"}</p>
-                    </div>
+                                    <h4 className="font-bold text-center capitalize text-2xl mb-4">{name}</h4>
 
-                    {cart.length > 0 ? (
-                        <div className="">
-                            <ul className="h-[550px] overflow-scroll my-4 bg-gray-100">
-                                {cart.map(({id, image, name, price, amount }, index) => (
-                                    <div key={`${name}-${index}`} className="border-b-2 p-4">
-
-                                        <h4 className="font-bold text-center capitalize">{name}</h4>
-
-                                        <div className="flex flex-row justify-between items-center">
+                                    <div className="flex flex-row justify-center items-center flex-wrap gap-4">
+                                        <div className="flex justify-between items-center w-full">
                                             <img className="h-16 w-16 object-cover" src={image} alt={name}/>
 
                                             {/*BUTTON AJOUT */}
@@ -80,23 +92,25 @@ export const ShoppingCart = ({cart, updateCart}) =>{
                                                 <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
                                                     <button data-action="decrement"
                                                             className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
-                                                    <span className="m-auto text-2xl font-thin"
-                                                          onClick={() => handleDecrement(id, image, name, price)}
-                                                    >−</span>
+                                                        <span className="m-auto text-2xl font-thin"
+                                                              onClick={() => handleDecrement(id, image, name, price)}
+                                                        >−</span>
                                                     </button>
                                                     <span className="justify-center w-full bg-gray-300 font-semibold focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none"
                                                     >{amount}</span>
                                                     <button data-action="increment"
                                                             className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
-                                                    <span className="m-auto text-2xl font-thin"
-                                                          onClick={() =>handleIncrement(id, image, name, price)}
-                                                    >+</span>
+                                                        <span className="m-auto text-2xl font-thin"
+                                                              onClick={() =>handleIncrement(id, image, name, price)}
+                                                        >+</span>
                                                     </button>
                                                 </div>
                                             </div>
                                             {/*BUTTON AJOUT */}
+                                        </div>
 
-                                            <span className="font-bold text-xl self-center">{price} €</span>
+                                        <div className="w-1/2 flex justify-between self-center">
+                                            <span className="font-bold text-xl self-center">{price.toFixed(2)} €</span>
                                             <button onClick={()=>{
                                                 let cartList = cart;
                                                 console.log(cart)
@@ -106,50 +120,48 @@ export const ShoppingCart = ({cart, updateCart}) =>{
                                             </button>
                                         </div>
                                     </div>
-                                ))}
-                            </ul>
-                            <div className=" my-2 ">
-                                <div className="flex flex-rox justify-between p-4">
-                                    <p>Produits ({cart.length})</p>
-                                    <p>{((total/121)*100)} €</p>
                                 </div>
-                                <div className="flex flex-rox justify-between p-4">
-                                    <p>TVA</p>
-                                    <p>{((total/121)*21)} €</p>
-                                </div>
-                                <div className="flex flex-rox justify-between p-4 border-t-2 text-2xl font-bold">
-                                    <p>Total à payer</p>
-                                    <p>{total} €</p>
-                                </div>
+                            ))}
+                        </ul>
+                        <div className=" my-2 ">
+                            <div className="flex flex-rox justify-between p-4">
+                                <p>Produits ({cart.length})</p>
+                                <p>{((total/121)*100).toFixed(2)} €</p>
+                            </div>
+                            <div className="flex flex-rox justify-between p-4">
+                                <p>TVA</p>
+                                <p>{((total/121)*21).toFixed(2)} €</p>
+                            </div>
+                            <div className="flex flex-rox justify-between p-4 border-t-2 text-2xl font-bold">
+                                <p>Total à payer</p>
+                                <p>{total.toFixed(2)} €</p>
                             </div>
                         </div>
-                    ) : (
-                        <div className="flex flex-row py-6">
-                            <img className="p-1.5 h-10 w-10 bg-blue-drop rotate-180 rounded-r-lg" src={attention} alt="attentionSign"/>
-                            <p className="px-4 py-2 bg-blue-drop-light w-full">Votre panier est vide</p>
-                        </div>
-                    )}
+                    </div>
+                ) : (
+                    <div className="flex flex-row py-6">
+                        <img className="p-1.5 h-10 w-10 bg-blue-drop rotate-180 rounded-r-lg" src={attention} alt="attentionSign"/>
+                        <p className="px-4 py-2 bg-blue-drop-light w-full">Votre panier est vide</p>
+                    </div>
+                )}
 
-                    <button className="font-bold w-full
+                <button className="font-bold w-full
                                 bg-button-return  bg-cover border border-gray-300 text-white
                                  hover:text-bold focus:ring-4 focus:ring-gray-200
                                  rounded-lg text-xl px-5 py-2.5 mr-2 mb-2
                                  ">
-                        Commander maintenant
-                    </button>
+                    Commander maintenant
+                </button>
 
-                    <button
-                        onClick={()=>updateCart([])}
-                        className="empty cart  text-sm  py-2.5  mb-2">
-                        Vider le panier
-                    </button>
-                </div>
-
+                <button
+                    onClick={()=>updateCart([])}
+                    className="empty cart  text-sm  py-2.5  mb-2">
+                    Vider le panier
+                </button>
             </div>
-        </>
-    ):  (<div >
-                <img alt ="Shopping Cart" className="relative h-8 cursor-pointer" src={cartImg} onClick={()=>setIsCartOpen(true)}/>
-                {cart.length > 0 ? <span className="absolute shadow-sm shadow-black bottom-5 bg-green-logo-light h-5 w-5 rounded-full flex flex-row justify-center">{cart.length}</span> : null}
+
         </div>
-    )
+
+        </>
+
 }
