a1227e7a3c55393120f7d8900d9adf3ca92f45a4
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-11-21 11:22:04 +0100

    page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,20 @@
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
+
 export const NotFound = () =>{
+    const navigate = useNavigate()
+
+    useEffect(() =>{
+        setTimeout(() => {
+            navigate("/")
+            // navigate(-1)
+        }, 2000)}, [])
+
     return(
-        <>
-        <h3>Not found</h3>
-        </>
+        <div className="h-[33vh] flex flex-col justify-center gap-8">
+            <h3 className="text-5xl self-center ">Page not found</h3>
+            <p className="text-xl self-center">You are being redirected</p>
+        </div>
     )
 }
